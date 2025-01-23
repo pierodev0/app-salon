93a292331e2cfee804b5c4114a36cd837c428f39
Piero Dev <pierodev21@gmail.com>
2025-01-23 00:07:29 -0500

    crear rutas para auth

---
+++ controllers/LoginController.php
@@ -0,0 +1,25 @@
+<?php
+namespace Controllers;
+class LoginController{
+
+    public static function index(){
+        echo "Desde login    ";
+    }
+
+    public static function logout(){
+        echo "Desde logout    ";
+    }
+
+    public static function olvide(){
+        echo "Desde olvide    ";
+    }
+
+    public static function recuperar(){
+        echo "Desde recuperar    ";
+    }
+
+    public static function crear(){
+        echo "Desde crear    ";
+    }
+
+}
--- public/index.php
+++ public/index.php
@@ -2,12 +2,27 @@
 
 require_once __DIR__ . '/../includes/app.php';
 
+use Controllers\LoginController;
 use MVC\Router;
 
 $router = new Router();
 
+//Iniciar Sesion
+$router->get("/",[LoginController::class, 'index']);
+$router->post("/",[LoginController::class, 'index']);
+$router->get("/logout",[LoginController::class, 'logout']);
+
+//Recuperar Password
+$router->get("/olvide",[LoginController::class, 'olvide']);
+$router->post("/olvide",[LoginController::class, 'olvide']);
+$router->get("/recuperar",[LoginController::class, 'recuperar']);
+$router->post("/recuperar",[LoginController::class, 'recuperar']);
 
 
+//Crear cuenta
+
+$router->get("/crear-cuenta",[LoginController::class, 'crear']);
+$router->post("/crear-cuenta",[LoginController::class, 'crear']);
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
