74a2c88b9b31b84ba4f8d84fcc5966521add9a73
Piero Dev <pierodev21@gmail.com>
2025-01-22 23:54:48 -0500

    start project

---
+++ Router.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace MVC;
+
+class Router
+{
+    public array $getRoutes = [];
+    public array $postRoutes = [];
+
+    public function get($url, $fn)
+    {
+        $this->getRoutes[$url] = $fn;
+    }
+
+    public function post($url, $fn)
+    {
+        $this->postRoutes[$url] = $fn;
+    }
+
+    public function comprobarRutas()
+    {
+        
+        // Proteger Rutas...
+        if(!isset($_SESSION)){
+            session_start();
+        }
+
+        // Arreglo de rutas protegidas...
+        // $rutas_protegidas = ['/admin', '/propiedades/crear', '/propiedades/actualizar', '/propiedades/eliminar', '/vendedores/crear', '/vendedores/actualizar', '/vendedores/eliminar'];
+
+        // $auth = $_SESSION['login'] ?? null;
+
+        $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        if ($method === 'GET') {
+            $fn = $this->getRoutes[$currentUrl] ?? null;
+            
+        } else {
+            $fn = $this->postRoutes[$currentUrl] ?? null;
+        }
+
+
+        if ( $fn ) {
+            if (is_callable($fn)) {
+                call_user_func($fn, $this);
+             } else {
+                echo "No existe el metodo " . $fn[1] . " en el controlador : " . $fn[0];
+             }
+        } else {
+            echo "Página No Encontrada o Ruta no válida";
+        }
+    }
+
+    public function render($view, $datos = [])
+    {
+
+        // Leer lo que le pasamos  a la vista
+        foreach ($datos as $key => $value) {
+            $$key = $value;  // Doble signo de dolar significa: variable variable, básicamente nuestra variable sigue siendo la original, pero al asignarla a otra no la reescribe, mantiene su valor, de esta forma el nombre de la variable se asigna dinamicamente
+        }
+
+        ob_start(); // Almacenamiento en memoria durante un momento...
+
+        // entonces incluimos la vista en el layout
+        include_once __DIR__ . "/views/$view.php";
+        $contenido = ob_get_clean(); // Limpia el Buffer
+        include_once __DIR__ . '/views/layout.php';
+    }
+}
