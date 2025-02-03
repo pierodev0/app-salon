942b8031d513c186d0e35a5b5a42d17b40830e20
Piero Dev <pierodev21@gmail.com>
2025-02-03 12:23:22 -0500

    admin : js buscar por fechas

--- src/js/buscador.js
+++ src/js/buscador.js
@@ -1,1 +1,16 @@
-console.log("Buscador.js cargado");
+document.addEventListener('DOMContentLoaded', function() {
+    iniciarApp();
+})
+
+
+function iniciarApp() {
+    buscarPorFecha()
+}
+
+function buscarPorFecha(){
+    const fechaInput = document.querySelector("#fecha")
+    fechaInput.addEventListener('input', function(e){
+        const fechaSeleccionada = e.target.value;
+        window.location = `?fecha=${fechaSeleccionada}`
+    })
+}
